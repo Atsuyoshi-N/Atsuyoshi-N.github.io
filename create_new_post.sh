e08c051f828478e6a3b01357ad44aa8b6aa949a8
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2020-11-12 13:19:00 +0900

    Change blog post path

--- create_new_post.sh
+++ create_new_post.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [[ $1 = "" ]]; then
-  echo "ファイル名を入力してください"
+  echo "フォルダ名を入力してください"
   exit 1
 fi
 
 DATE=$(date "+%Y-%m-%dT%H:%M:%S+09:00")
 
-cat << EOF >> contents/blog/$1
+mkdir -p contents/blog/$1
+
+cat << EOF >> contents/blog/$1/index.md
 ---
 date: $DATE
 title:
