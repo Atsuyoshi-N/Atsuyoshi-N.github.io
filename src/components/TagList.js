c092808c380f5fd930e4763b88b101814dad83c2
Atsuyoshi-N <ash.sit.you7@ezweb.ne.jp>
2021-05-25 16:30:32 +0900

    Replace makeStyles with styled-component in TagList page

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,41 +1,39 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import kebabCase from 'lodash/kebabCase'
-import { makeStyles } from '@material-ui/core/styles'
-import { Typography } from '@material-ui/core'
+import styled from 'styled-components'
 import LocalOfferIcon from '@material-ui/icons/LocalOffer'
 
-const useStyles = makeStyles({
-  tagsList: {
-    listStyle: 'none',
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'flex-start',
-    margin: '10px 0',
-  },
-  tagItem: {
-    color: 'rgba(0, 0, 0, 0.54)',
-    margin: '0 5px 0',
-  },
-})
-
 export default function TagList(props) {
-  const classes = useStyles()
   const { frontmatter } = props
   return (
-    <ul className={classes.tagsList}>
-      <LocalOfferIcon fontSize="small" />
+    <TagLists>
+      <LocalOfferIcon fontSize="small" color="action" />
       {frontmatter.tags !== null &&
         frontmatter.tags.map(tag => (
-          <li>
-            <Typography variant="subtitle2" color="textSecondary">
-              <Link
-                to={`/tags/${kebabCase(tag)}`}
-                className={classes.tagItem}
-              >{`#${tag}`}</Link>
-            </Typography>
-          </li>
+          <Tag>
+            <Link to={`/tags/${kebabCase(tag)}`}>{`#${tag}`}</Link>
+          </Tag>
         ))}
-    </ul>
+    </TagLists>
   )
 }
+
+const TagLists = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin: 10px 0;
+`
+
+const Tag = styled.li`
+  a {
+    font-size: 0.8rem;
+    color: rgba(0, 0, 0, 0.54);
+    font-weight: 500;
+    letter-spacing: 0.00714em;
+    line-height: 1.57;
+    margin: 0 5px 0;
+  }
+`
